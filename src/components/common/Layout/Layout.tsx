370df4b33405cc44bd73144250fdf9715a32ca3f
Vladimir Turkonja <vladimir.turkonja@icloud.com>
2023-01-02 12:11:21 +0100

    Add max width to Layout and center it

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -8,13 +8,15 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <div className="flex flex-row">
-      <Sidebar />
-      <div className="w-full">
-        <Navabr />
-        <div className="flex w-full flex-col justify-between">
-          <main className="mb-12 lg:mb-0">{children}</main>
-          <BottomNavbar />
+    <div className="flex flex-row justify-center">
+      <div className="flex w-full max-w-7xl">
+        <Sidebar />
+        <div className="w-full">
+          <Navabr />
+          <div className="flex w-full flex-col justify-between">
+            <main className="mb-12 lg:mb-0">{children}</main>
+            <BottomNavbar />
+          </div>
         </div>
       </div>
     </div>
