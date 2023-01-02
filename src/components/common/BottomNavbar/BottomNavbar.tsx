68d517da422344d60a1f792588cdc6a88d45a478
Vladimir Turkonja <vladimir.turkonja@icloud.com>
2023-01-02 11:05:07 +0100

    Remove active icon state

--- src/components/common/BottomNavbar/BottomNavbar.tsx
+++ src/components/common/BottomNavbar/BottomNavbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 import {
   HomeIcon,
@@ -8,48 +7,36 @@ import {
   EnvelopeIcon,
 } from "@heroicons/react/24/outline";
 
-import {
-  HomeIcon as HomeIconSolid,
-  BellIcon as BellIconSolid,
-  EnvelopeIcon as EnvelopeIconSolid,
-} from "@heroicons/react/24/solid";
-
 /**
- * @TODO: revisit icons
+ * @TODO: revisit icons | add active state
  */
 const bottonNavLinks = [
   {
     icon: <HomeIcon className="h-6 w-6" />,
-    activeIcon: <HomeIconSolid className="h-6 w-6" />,
     href: "/",
   },
   {
     icon: <MagnifyingGlassIcon className="h-6 w-6" />,
-    activeIcon: <MagnifyingGlassIcon className="h-6 w-6" />,
     href: "/search",
   },
   {
     icon: <BellIcon className="h-6 w-6" />,
-    activeIcon: <BellIconSolid className="h-6 w-6" />,
     href: "/notifications",
   },
   {
     icon: <EnvelopeIcon className="h-6 w-6" />,
-    activeIcon: <EnvelopeIconSolid className="h-6 w-6" />,
     href: "/messages",
   },
 ];
 
 const BottomNavbar = () => {
-  const router = useRouter();
-
   const bottomItems = bottonNavLinks?.map((item) => (
     <Link
       key={item?.href}
       className="stroke flex w-full items-center justify-center py-3 text-sm"
       href={item?.href}
     >
-      {router.pathname === item?.href ? item?.activeIcon : item?.icon}
+      {item?.icon}
     </Link>
   ));
 
