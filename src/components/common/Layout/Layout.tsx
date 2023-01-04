512abd35f6318691f99c59454fe25ffb016e5c66
Vladimir Turkonja <vladimir.turkonja@icloud.com>
2023-01-04 11:40:12 +0100

    Adjust content position on different screen sizes

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -9,9 +9,9 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <div className="flex flex-row justify-center">
-      <div className="flex w-full max-w-7xl">
+      <div className="flex w-full max-w-7xl md:justify-center">
         <Sidebar />
-        <div className="w-full">
+        <div className="w-full border-r-[1px] md:w-[600px]">
           <Navabr />
           <div className="flex w-full flex-col justify-between">
             <main className="mb-12 lg:mb-0">{children}</main>
