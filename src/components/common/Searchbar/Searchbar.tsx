ff1b795ca09880efb4a380001563aa8eba654000
Vladimir Turkonja <vladimir.turkonja@icloud.com>
2023-01-04 11:41:26 +0100

    Create a Searchbar (WIP)

---
+++ src/components/common/Searchbar/Searchbar.tsx
@@ -0,0 +1,23 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+const Searchbar = () => {
+  return (
+    <div className="relative flex-1 rounded-full shadow-sm">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <MagnifyingGlassIcon
+          className="h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+      </div>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        className="text-md block w-full rounded-full bg-gray-100 py-1 pl-10 focus:outline-sky-500 lg:py-2"
+        placeholder="Search"
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
