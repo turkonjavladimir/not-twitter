f9c8e6702532b243a3402165470bcbb135903447
Vladimir Turkonja <vladimir.turkonja@icloud.com>
2023-01-08 18:26:26 +0100

    Adjust Avatar for dark mode

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -30,10 +30,10 @@ const Avatar = ({ label = "", size = "sm", imageUrl = "" }: AvatarProps) => {
 
   return (
     <span
-      className={`${sizeClasses} inline-flex items-center justify-center rounded-full border-2 border-neutral-100 bg-gradient-to-tl from-slate-300 transition-colors ease-linear hover:cursor-pointer md:text-sm`}
+      className={`${sizeClasses} inline-flex items-center justify-center rounded-full border-2 border-neutral-100 bg-gradient-to-tl from-slate-300 transition-colors ease-linear hover:cursor-pointer dark:border-neutral-700 dark:from-neutral-500 md:text-sm`}
     >
       {!imageUrl ? (
-        <span className="text-xs font-medium leading-none text-black">
+        <span className="text-xs font-medium leading-none text-black dark:text-neutral-200">
           {getInitials(label)}
         </span>
       ) : (
