c9beb16873aa347389c516a5be7e3099862b7158
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-01-28 11:34:16 +0100

    Adjust dropdown menu max height

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -43,7 +43,7 @@ const Dropdown = ({ children }: { children: React.ReactNode }) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 max-h-60 w-56 origin-top-right divide-y divide-gray-100 overflow-y-scroll rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:divide-neutral-700 dark:bg-neutral-800">
+        <Menu.Items className="absolute right-0 z-10 mt-2 max-h-52 w-56 origin-top-right divide-y divide-gray-100 overflow-y-scroll rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:divide-neutral-700 dark:bg-neutral-800">
           {children}
         </Menu.Items>
       </Transition>
