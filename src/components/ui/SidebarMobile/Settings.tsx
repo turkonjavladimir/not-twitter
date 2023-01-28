c360fe4d6a0efbb40c8b9201980263c5172570d2
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-01-28 20:58:43 +0100

    Refactor sidebar navigation

---
+++ src/components/ui/SidebarMobile/Settings.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+
+import { ManualThemeToggle, SystemThemeToggle } from "../";
+
+const Settings = () => {
+  return (
+    <div className="border-t border-neutral-200 p-1 dark:border-neutral-700">
+      <div className="my-2 px-3">
+        <span className="font-semibold">Settings</span>
+      </div>
+      <div className="ml-1 flex h-auto max-h-64 flex-shrink-0 flex-col overflow-y-auto">
+        <ManualThemeToggle />
+        <SystemThemeToggle />
+
+        <Link
+          href="/logout"
+          className="flex items-center justify-between px-2 py-2"
+        >
+          <div>
+            <span className="">Logout</span>
+          </div>
+          <div>
+            <ArrowRightOnRectangleIcon className="h-5 w-5" />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
--- src/components/ui/SidebarMobile/SidebarMobile.tsx
+++ src/components/ui/SidebarMobile/SidebarMobile.tsx
@@ -1,37 +1,27 @@
-import { Dialog, Transition } from "@headlessui/react";
-import React, { Fragment } from "react";
-import {
-  BellIcon,
-  BookmarkIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  EllipsisHorizontalCircleIcon,
-  EnvelopeIcon,
-  FolderIcon,
-  HomeIcon,
-  InboxIcon,
-  MagnifyingGlassIcon,
-  PlusCircleIcon,
-  QueueListIcon,
-  UserIcon,
-  UsersIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { Avatar } from "../../common/Avatar";
-import { LockClosedIcon } from "@heroicons/react/24/solid";
-import Toggle from "../Toggle";
-import { ThemeSwitcher, ManualThemeSwitcher } from "../ThemeSwitcher";
-import ThemeIcon from "../ThemeSwitcher/ThemeIcon";
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  UserIcon,
+  XMarkIcon,
+  BookmarkIcon,
+  QueueListIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+} from "@heroicons/react/24/outline";
+
 import { useUI } from "../context";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import Settings from "./Settings";
+import AccountInfo from "./AccountInfo";
 
 const sideNavLinks = [
   {
-    icon: <HomeIcon className="h-6 w-6" />,
+    icon: <UserIcon className="h-6 w-6" />,
+    url: "/profile",
+    label: "Profile",
+  },
+  {
+    icon: <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />,
     url: "/topics",
     label: "Topics",
   },
@@ -45,26 +35,6 @@ const sideNavLinks = [
     url: "/lists",
     label: "Lists",
   },
-  {
-    icon: <EnvelopeIcon className="h-6 w-6" />,
-    url: "/messages",
-    label: "Messages",
-  },
-  {
-    icon: <BookmarkIcon className="h-6 w-6" />,
-    url: "/bookmarks",
-    label: "Bookmarks",
-  },
-  {
-    icon: <UserIcon className="h-6 w-6" />,
-    url: "/profile",
-    label: "Profile",
-  },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
 ];
 
 type NavItemProps = {
@@ -72,6 +42,7 @@ type NavItemProps = {
   active?: boolean;
   icon?: React.ReactNode;
 };
+
 const NavItem = ({ label, icon, active }: NavItemProps) => {
   return (
     <span
@@ -79,11 +50,9 @@ const NavItem = ({ label, icon, active }: NavItemProps) => {
         active ? "bg-gray-100 dark:bg-neutral-700" : ""
       } flex w-full items-center justify-start rounded-lg p-3 text-sm transition-colors hover:bg-gray-100 dark:hover:bg-neutral-700 lg:justify-start`}
     >
-      <div className="flex items-center gap-4">
+      <div className="flex min-w-0 items-center gap-4">
         <div>{icon}</div>
-        <div>
-          <span className="text-lg">{label}</span>
-        </div>
+        <span className="truncate text-lg">{label}</span>
       </div>
     </span>
   );
@@ -91,6 +60,16 @@ const NavItem = ({ label, icon, active }: NavItemProps) => {
 
 const SidebarMobile = ({ displaySidebar }: { displaySidebar: boolean }) => {
   const { closeSidebar } = useUI();
+  const sidebarNavItems = sideNavLinks?.map((item) => (
+    <Link href={"/"} key={item?.url}>
+      <NavItem
+        icon={item?.icon}
+        label={item?.label}
+        /*   active={active} */
+      />
+    </Link>
+  ));
+
   return (
     <div>
       <Transition.Root show={displaySidebar} as={Fragment}>
@@ -122,125 +101,33 @@ const SidebarMobile = ({ displaySidebar }: { displaySidebar: boolean }) => {
               leaveTo="-translate-x-full"
             >
               <Dialog.Panel className="relative flex w-full max-w-[280px] flex-1 flex-col bg-white pt-3 dark:bg-neutral-800">
-                <div className="flex flex-shrink-0 items-center justify-between px-4">
-                  <div>
-                    <span className="font-semibold">Account info</span>
-                  </div>
-                  <div className="">
-                    <button
-                      type="button"
-                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full"
-                      onClick={closeSidebar}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                  </div>
+                <div className="flex min-w-0 flex-shrink-0 items-center justify-between px-4">
+                  <span className="truncate font-semibold">Account info</span>
+
+                  <button
+                    type="button"
+                    className="flex items-center justify-center rounded-full p-2 outline-none"
+                    onClick={closeSidebar}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                  </button>
                 </div>
 
                 <div className="overflow-y-auto">
-                  <div className="p-4">
-                    <div className="flex w-full items-center justify-between">
-                      <div>
-                        <Avatar size="md" label="Name Surname" />
-                      </div>
-                      <Link href="/">
-                        <PlusCircleIcon className="h-8 w-8 text-neutral-400" />
-                      </Link>
-                    </div>
-
-                    <div className="mt-2">
-                      <Link
-                        href="/"
-                        className="flex min-w-0 items-center gap-2 hover:underline"
-                      >
-                        <span className="truncate font-semibold">
-                          Name Surname
-                        </span>
-                        <div className="shrink-0">
-                          <LockClosedIcon className="h-4 w-4" />
-                        </div>
-                      </Link>
-
-                      <div className="flex min-w-0 leading-none">
-                        <span className="truncate text-sm text-neutral-500">
-                          @username
-                        </span>
-                      </div>
-
-                      <div className="mt-3 flex gap-5">
-                        <div>
-                          <Link
-                            href="/"
-                            className="flex gap-1 text-sm hover:underline"
-                          >
-                            <span className="font-semibold">31</span>
-                            <span className="text-neutral-500">Following</span>
-                          </Link>
-                        </div>
-                        <div>
-                          <Link
-                            href="/"
-                            className="flex gap-1 text-sm hover:underline"
-                          >
-                            <span className="font-semibold">0</span>
-                            <span className="text-neutral-500">Followers</span>
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <AccountInfo
+                    name="Name Surname"
+                    username="username"
+                    avatarLabel="Name Surname"
+                    followers={"10230"}
+                    following={"112949999"}
+                  />
 
                   <div className="h-auto flex-1 p-1">
-                    <nav className="space-y-1">
-                      {sideNavLinks?.map((item) => (
-                        /*         <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-indigo-800 text-white"
-                            : "text-indigo-100 hover:bg-indigo-600",
-                          "group flex items-center rounded-md px-2 py-2 text-base font-medium"
-                        )}
-                      >
-                        <item.icon
-                          className="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300"
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </a> */
-
-                        <Link href={"/"} key={item?.url}>
-                          <NavItem
-                            icon={item?.icon}
-                            label={item?.label}
-                            /*   active={active} */
-                          />
-                        </Link>
-                      ))}
-                    </nav>
+                    <nav className="space-y-1">{sidebarNavItems}</nav>
                   </div>
 
-                  <div className="border-t border-neutral-200 p-4 dark:border-neutral-700">
-                    <div className="mb-3">
-                      <span className="font-semibold">Settings</span>
-                    </div>
-                    <div className="flex h-auto max-h-64 flex-shrink-0 flex-col gap-1 overflow-y-auto">
-                      <div className="flex items-center justify-between">
-                        <span>Theme</span>
-                        <div>
-                          <ManualThemeSwitcher />
-                        </div>
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <span>Use system theme</span>
-                        <div>
-                          <ThemeSwitcher />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <Settings />
                 </div>
               </Dialog.Panel>
             </Transition.Child>
