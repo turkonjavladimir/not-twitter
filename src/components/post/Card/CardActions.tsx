6d833ab3ef7f7ad9764db7b5d5b9678a80d49e38
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-01-29 18:19:06 +0100

    Adjust spacings and color

--- src/components/post/Card/CardActions.tsx
+++ src/components/post/Card/CardActions.tsx
@@ -7,23 +7,23 @@ import {
 
 const CardActions = () => {
   return (
-    <div className="mt-3 mb-3 flex gap-3">
+    <div className="my-3 flex max-w-md justify-between gap-3">
       <button className="min-w-0" aria-label="reply to tweet">
-        <div className="flex min-w-0 items-center gap-1 rounded-full transition-colors hover:text-sky-600">
+        <div className="flex min-w-0 items-center gap-1 rounded-full text-neutral-500 transition-colors hover:text-sky-600">
           <ChatBubbleOvalLeftIcon className="h-5 w-5 shrink-0" />
           <span className="truncate text-xs">1,669</span>
         </div>
       </button>
 
       <button className="min-w-0" aria-label="retweet">
-        <div className="flex items-center gap-1 transition-colors hover:text-green-600">
+        <div className="flex items-center gap-1 text-neutral-500 transition-colors hover:text-green-600 ">
           <ArrowPathRoundedSquareIcon className="h-5 w-5 shrink-0" />
           <span className="truncate text-xs">14.9k</span>
         </div>
       </button>
 
       <button className="min-w-0" aria-label="like tweet">
-        <div className="flex items-center gap-1 transition-colors hover:text-red-600">
+        <div className="flex items-center gap-1 text-neutral-500 transition-colors hover:text-red-600 ">
           <HeartIcon className="h-5 w-5 shrink-0" />
           <span className="truncate text-xs">359</span>
         </div>
@@ -31,7 +31,7 @@ const CardActions = () => {
 
       <button
         aria-label="share tweet"
-        className="min-w-0 transition-colors hover:text-blue-500"
+        className="min-w-0 text-neutral-500 transition-colors hover:text-blue-500 "
       >
         <ShareIcon className="h-5 w-5 shrink-0" />
       </button>
